4f6741c04b505c7144923253efb01463f2470d6c
Renan Fagundes <renanbigm@gmail.com>
2024-03-18 12:52:03 -0300

    creating UI with react

--- restApi/services/issuesServices.js
+++ restApi/services/issuesServices.js
@@ -23,31 +23,39 @@ const createIssue = (title, description) => {
     const issues = getAllIssues();
     const issueID = issues.length + 1;
     const newIssue = { id: issueID, title, description };
+
     issues.push(newIssue);
     saveDataToFile(issues);
+
     return newIssue;
 };
 
 const updateIssue = (id, title, description) => {
     let issues = getAllIssues();
     const index = issues.findIndex(issue => issue.id === id);
+
     if (index === -1) {
         throw new Error('Issue not found');
     }
+
     issues[index].title = title || issues[index].title;
     issues[index].description = description || issues[index].description;
     saveDataToFile(issues);
+
     return issues[index];
 };
 
 const deleteIssue = (id) => {
     let issues = getAllIssues();
     const index = issues.findIndex(issue => issue.id === id);
+
     if (index === -1) {
         throw new Error('Issue not found');
     }
+
     const deletedIssue = issues.splice(index, 1)[0];
     saveDataToFile(issues);
+
     return deletedIssue;
 };
 
